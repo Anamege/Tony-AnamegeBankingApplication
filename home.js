4e7d27b806c88e91b770fed84d605e7aae5a9b49
Anamege <87146339+Anamege@users.noreply.github.com>
2022-01-07 16:51:07 -0500

    Create home.js

---
+++ home.js
@@ -0,0 +1,14 @@
+function Home() {
+    return (
+      <Card
+        bgcolor="success"
+        txtcolor="white"
+        header="Bad Bank Landing Page"
+        title="Welcome To Bad Bank"
+        body={
+          <img src="bank6.jpg" className="img-fluid" alt="Responsive Image" />
+        }
+      />
+    );
+  }
+  
