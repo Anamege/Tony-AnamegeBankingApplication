f1f43f71f6e040fe80d5e8007f9347fdadbee84f
Anamege <87146339+Anamege@users.noreply.github.com>
2022-01-08 22:26:43 -0500

    Update context.js

--- context.js
+++ context.js
@@ -52,7 +52,7 @@ function Card(props){
 
     
     return (
-        <div className={classes()} style={{maxWidth: "18rem"}}>
+        <div className={classes()} style={{maxWidth: "40rem"}}>
             <div className="card-header">{props.header}</div>
             <div className="card-body">
                 {props.title && (<h4 className="card-title">{props.title}</h4>)}
