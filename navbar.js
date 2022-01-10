ccdfc61e5b06f8aafe21755606a718d0d817a11e
Anamege <87146339+Anamege@users.noreply.github.com>
2022-01-10 12:05:31 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -4,28 +4,28 @@ function NavBar() {
 
       <nav className="navbar navbar-expand-lg navbar-light bg-warning">
     
-      <a className="navbar-brand" href="#"><span class="mb-0 h3">Front End Bank</span></a>
+      <a className="navbar-brand" href="#"><span class="mb-0 h3"><b>Front End Bank</b></span></a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav mx-auto">
               <li className="nav-item">
-                  <a className="nav-link" href="#/CreateAccount/">Create Account</a>
+                  <a className="nav-link" href="#/CreateAccount/"><b>Create Account</b></a>
                   </li>
                   <li className="nav-item">
-                  <a className="nav-link" href="#/login/">Login</a>
+                  <a className="nav-link" href="#/login/"><b>Login</b></a>
                   </li>
                   <li className="nav-item">
-                  <a className="nav-link" href="#/deposit/">Deposit</a>
+                  <a className="nav-link" href="#/deposit/"><b>Deposit</b></a>
                   </li>  
   
                    <li className="nav-item">
-                  <a className="nav-link" href="#/withdraw/">Withdraw</a>
+                  <a className="nav-link" href="#/withdraw/"><b>Withdraw</b></a>
                   </li>                
 
                   <li className="nav-item">
-                  <a className="nav-link" href="#/AllData/">All Data</a>
+                  <a className="nav-link" href="#/AllData/"><b>All Data</b></a>
                   </li>
           </ul>  
   
