dcc65fedc1b5f3fc7b9a5c18a13e7db6dd76b6c9
Anamege <87146339+Anamege@users.noreply.github.com>
2022-01-19 00:23:17 -0500

    Update createaccount.js

--- createaccount.js
+++ createaccount.js
@@ -5,37 +5,39 @@ function CreateAccount() {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const ctx = React.useContext(UserContext);
-  
 
   //Validate function
   function validate(field, label) {
     if (!field) {
-      setStatus("Error: " + label);
-      setTimeout(() => setStatus(""), 3000);
+      setStatus('Error: Please enter valid ' + label);
+      setTimeout(() => setStatus(''), 3000);
       return false;
     }
     return true;
   }
 
-//Validate Account Details
+  //Validate Account Details
   function handleCreate() {
     console.log(name, email, password);
-    if (!validate(name, "name")) return;    
-    if (!validate(email, "email")) return;
-    if (!validate(password, "password")) return;
-    ctx.users.push({name, email, password, balance: 100 });
+    if (!validate(name, 'name')) return;
+    if (!validate(email, 'email')) return;
+    if (!validate(password, 'password')) return;
+    if(password.length < 8){
+      alert("Must contain at least 8 characters including a special caharcter, one number and one uppercase and lowercase letter")
+      return false}
+    ctx.users.push({ name, email, password, balance: 100 });
     setShow(false);
   }
 
-//Clear & Reset Form After Account Creation
+  //Clear & Reset Form After Account Creation
   function clearForm() {
     setName("");
     setEmail("");
     setPassword("");
     setShow(true);
   }
-  
-//Style With Bootstrap Card
+
+  //Style With Bootstrap Card
   return (
     <Card
       bgcolor="success"
@@ -73,10 +75,10 @@ function CreateAccount() {
               className="form-control"
               id="password"
               placeholder="Enter Password"
-              value={password}
+              value={password}     
               onChange={(e) => setPassword(e.currentTarget.value)}
-            />
-            <br />
+              />              
+           
             <button
               type="submit"
               className="btn btn-danger"
@@ -86,15 +88,20 @@ function CreateAccount() {
             </button>
           </>
         ) : (
-
           //Validate Name, Email Address, Password Successfully Created
-          <>      
-            <h5>Success! Account Created.</h5><br/>
-            <button type="submit" className="btn btn-danger" onClick={clearForm}>Add Another Account</button>
+          <>
+            <h5>Success! Account Created.</h5>
+            <br />
+            <button
+              type="submit"
+              className="btn btn-danger"
+              onClick={clearForm}
+            >
+              Add Another Account
+            </button>
           </>
         )
       }
     />
   );
 }
-//
